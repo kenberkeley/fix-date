a275fe5f5329916b624431f5f52ea39fb9db11a7
Ken Berkeley <kenberkeley@foxmail.com>
2017-07-13 23:38:49 +0800

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+if (isNaN(Date.parse('2000-01-01 00:00:00'))) {
+  const _Date = Date
+
+  const standardizeArgs = args => {
+    if (args.length === 1 && typeof args[0] === 'string') {
+      args[0] = args[0].replace(/-/g, '/')
+    }
+    return args
+  }
+
+  function $Date() {
+    if (this instanceof $Date) {
+      return new _Date(...standardizeArgs(arguments))
+    }
+    return _Date(...arguments)
+  }
+  $Date.prototype = _Date.prototype
+  
+  $Date.now = _Date.now
+  $Date.UTC = _Date.UTC
+  $Date.parse = function () {
+    return _Date.parse(...standardizeArgs(arguments))
+  }
+
+  Date = $Date
+}
