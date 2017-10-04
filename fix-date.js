bd1d54681e3467b93f3d032c06702508c1dfc2db
Ken Berkeley <kenberkeley@foxmail.com>
2017-10-04 16:39:58 +0800

    [fix] https://github.com/kenberkeley/fix-date/pull/1

--- fix-date.js
+++ fix-date.js
@@ -1,30 +1,28 @@
 !function (_Date) {
-  if (isNaN(_Date.parse('2000-01-01 00:00:00'))) {
-    function standardizeArgs(args) {
-      if (args.length === 1 && typeof args[0] === 'string') {
-        // '2000-01-01 00:00:00' => '2000/01/01 00:00:00'
-        args[0] = args[0].replace(/-/g, '/')
-      }
-      return Array.prototype.slice.call(args)
+  function standardizeArgs(args) {
+    if (args.length === 1 && typeof args[0] === 'string' && isNaN(_Date.parse(args[0]))) {
+      // '2000-01-01 00:00:00' => '2000/01/01 00:00:00'
+      args[0] = args[0].replace(/-/g, '/')
     }
-
-    function $Date() {
-      if (this instanceof $Date) {
-        // hijack constructor
-        return new (
-          Function.prototype.bind.apply(_Date, [null].concat(standardizeArgs(arguments)))
-        )()
-      }
-      return _Date()
-    }
-    $Date.prototype = _Date.prototype
-    
-    $Date.now = _Date.now
-    $Date.UTC = _Date.UTC
-    $Date.parse = function () {
-      return _Date.parse.apply(_Date, standardizeArgs(arguments))
-    }
-
-    Date = $Date
+    return Array.prototype.slice.call(args)
   }
+
+  function $Date() {
+    if (this instanceof $Date) {
+      // hijack constructor
+      return new (
+        Function.prototype.bind.apply(_Date, [null].concat(standardizeArgs(arguments)))
+      )()
+    }
+    return _Date()
+  }
+  $Date.prototype = _Date.prototype
+  
+  $Date.now = _Date.now
+  $Date.UTC = _Date.UTC
+  $Date.parse = function () {
+    return _Date.parse.apply(_Date, standardizeArgs(arguments))
+  }
+
+  Date = $Date
 }(Date);
